7105cfc4d18c735a3c16404d872593003d9f9caf
unknown <mynamerahulkumar13@gmail.com>
2024-08-13 09:34:02 +0100

    updated pom

---
+++ algotradingapi/dhanapi/orderApi1.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+clientid=1103695755
+
+access_token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzI0MzUxMjk5LCJ0b2tlbkNvbnN1bWVyVHlwZSI6IlNFTEYiLCJ3ZWJob29rVXJsIjoiIiwiZGhhbkNsaWVudElkIjoiMTEwMzY5NTc1NSJ9.0mOPgx7lfTGrAjA0FymdtldZmPEW8wIKe2XLrGxk_Hb6utx3m5sC5qIO4z3eMVXvgEnGo962iFcgFA15aI3hEg"
+
+
+
+
+
+
+
+
+"""
+Created on Tue Aug 13 09:27:02 2024
+
+@author: LENOVO
+"""
+
+
+from dhanhq import dhanhq
+import pandas as pd
+
+dhan = dhanhq(clientid,access_token)
+def get_positions(dhan):
+    positions=dhan.get_positions()
+    if positions is not None:
+       df_position=pd.DataFrame(positions['data'])
+       return df_position
+    return  "NO Positions"
+def get_holding(dhan):
+    holdings=dhan.get_holdings()
+    print(holdings)
+
+
+
+def get_orders(dhan):
+    data = dhan.get_order_list()
+    if data is not None:
+        positions =  pd.DataFrame(data['data'])
+        print(positions)
+        return positions
+    return "No Orders"
+positions=get_positions(dhan)
+holdings=get_holding(dhan)
+orders=get_orders(dhan)
+print(orders)
